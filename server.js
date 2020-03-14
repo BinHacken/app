2ceff0ed18234b5d65269be6ae59ba14310d8eff
Spacehuhn <github@spacehuhn.com>
2020-03-14 18:14:46 +0100

    Login using cookie

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const args = process.argv.slice(2);
 
 // ===== Helper functions ===== //
 
-function check_login(req, res) {
+function loggedin(req, res, url) {
   if (req.session.loggedin) {
     return true;
   } else {
-    res.redirect('/login');
+    res.redirect(`/login?url=${url}`);
     return false;
   }
 }
@@ -112,10 +112,54 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-  res.render('login', {
-    nope: req.query.nope,
-    loggedin: req.session.loggedin
+  var url = req.query.url;
+  url = url ? url : '/home';
+
+  if (req.session.loggedin) {
+    res.redirect(`/${url}`);
+    return;
+  }
+
+  var cookie = getCookie(req);
+
+  var renderPage = function() {
+    res.render('login', {
+      nope: req.query.nope,
+      loggedin: req.session.loggedin,
+      url: url
+    });
+  }
+
+  if (!cookie.username || !cookie.sid || !cookie.token) {
+    renderPage();
+    return;
+  }
+  /*
+    console.log({
+      "Username": username,
+      "sid": sid,
+      "token": token
+    });
+  */
+  db.authSession(cookie.username, cookie.sid, cookie.token).then((session) => {
+    if (session && session.token) {
+      console.log("Authenticated!");
+      console.log(`Setting new token ${session.token}`);
+
+      setCookie(res, {
+        token: session.token
+      });
+
+      req.session.loggedin = true;
+      req.session.username = cookie.username;
+
+      res.redirect(`/${url}`);
+    } else {
+      deleteCookies(res);
+      renderPage();
+    }
   });
+
 });
 
 app.get('/register', function(req, res) {
@@ -127,43 +171,43 @@ app.get('/register', function(req, res) {
 });
 
 app.get('/home', function(req, res) {
-  if (check_login(req, res)) {
-    res.render('home', {
-      loggedin: req.session.loggedin
-    });
-  }
+  if (!loggedin(req, res, 'home')) return;
+
+  res.render('home', {
+    loggedin: req.session.loggedin
+  });
 });
 
 app.get('/users', function(req, res) {
-  if (check_login(req, res)) {
-    db.getUserList().then((data) => {
-      res.render('users', {
-        loggedin: req.session.loggedin,
-        users: data
-      });
+  if (!loggedin(req, res, 'users')) return;
+
+  db.getUserList().then((data) => {
+    res.render('users', {
+      loggedin: req.session.loggedin,
+      users: data
     });
-  }
+  });
 });
 
 app.get('/profile', function(req, res) {
-  if (check_login(req, res)) {
-    db.getUserData(req.session.username).then((data) => {
-      res.render('profile', {
-        loggedin: req.session.loggedin,
-        user: data,
-        res: req.query.res
-      });
+  if (!loggedin(req, res, 'profile')) return;
+
+  db.getUserData(req.session.username).then((data) => {
+    res.render('profile', {
+      loggedin: req.session.loggedin,
+      user: data,
+      res: req.query.res
     });
-  }
+  });
 });
 
 app.get('/logout', function(req, res) {
-  req.session.destroy();
-  res.redirect('/login');
-});
+  var cookie = getCookie(req);
 
-app.get('/logout', function(req, res) {
   req.session.destroy();
+  deleteCookies(res);
+  db.deleteSession(cookie.username, cookie.sid);
+
   res.redirect('/login');
 });
 
@@ -171,16 +215,32 @@ app.get('/logout', function(req, res) {
 app.post('/auth', function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
+  var url = req.body.url;
+  url = url ? url : '/home';
 
-  console.log(`Login "${username}","${password}"`);
+  console.log('Login ');
+  console.log({
+    username: username,
+    password: '***',
+    url: url
+  });
 
   db.auth(username, password).then((success) => {
     if (success) {
       req.session.loggedin = true;
       req.session.username = username;
-      res.redirect('/home');
+
+      db.createSession(username).then((user) => {
+        setCookie(res, {
+          username: user.username,
+          sid: user.sid,
+          token: user.token
+        });
+      }).then(() => {
+        res.redirect(`/${url}`);
+      });
     } else {
-      res.redirect('/login?nope=Access denied :(');
+      res.redirect(`/login?url=${url}&nope=Access denied : (`);
     }
   });
 });
@@ -191,7 +251,9 @@ app.post('/register', function(req, res) {
   var tan = req.body.tan;
 
   if (username && password && tanlist.check(tan)) {
-    console.log(`Register "${username}"`);
+    console.log(`
+              Register "${username}"
+              `);
 
     db.createUser(username, password).then(() => {
       req.session.loggedin = true;
@@ -206,65 +268,77 @@ app.post('/register', function(req, res) {
 });
 
 app.post('/update-name', function(req, res) {
-  if (check_login(req, res)) {
-    var old_username = req.session.username;
-    var new_username = req.body.username;
+  if (!loggedin(req, res, 'update-name')) return;
 
-    db.exists(new_username).then((exists) => {
-      if (!exists) {
-        db.updateUser(old_username, 'name', new_username).then(() => {
-          req.session.username = new_username;
-          res.redirect(`/profile?res=Benutzername geändert zu ${new_username}`);
-        });
-      } else {
-        res.redirect(`/profile?res=Benutzername schon vergeben`);
-      }
+  var old_username = req.session.username;
+  var new_username = req.body.username;
+
+  db.exists(new_username).then((exists) => {
+    if (!exists) return db.updateUser(old_username, 'name', new_username);
+    else throw new Error('Benutzername schon vergebem');
+  }).then(() => {
+    req.session.username = new_username;
+
+    setCookie(res, {
+      username: new_username
     });
-  }
+
+    return db.renameSession(old_username, new_username);
+  }).then(() => {
+    res.redirect(` / profile ? res = Benutzername geändert zu $ {
+                new_username
+              }
+              `);
+  }).catch((msg) => {
+    res.redirect(` / profile ? res = $ {
+                msg
+              }
+              `);
+  });
 });
 
 app.post('/update-data', function(req, res) {
-  if (check_login(req, res)) {
-    var username = req.session.username;
-    var new_data = req.body.data;
+  if (!loggedin(req, res, 'update-data')) return;
 
-    db.updateUser(username, 'data', new_data).then(() => {
-      res.redirect(`/profile?res=Beschreibung geändert`);
-    });
-  }
+  var username = req.session.username;
+  var new_data = req.body.data;
+
+  db.updateUser(username, 'data', new_data).then(() => {
+    res.redirect(` / profile ? res = Beschreibung geändert `);
+  });
 });
 
 app.post('/update-pswd', function(req, res) {
-  if (check_login(req, res)) {
-    var username = req.session.username;
-    var old_password = req.body.password_old;
-    var new_password = req.body.password_new;
+  if (!loggedin(req, res, 'update-pswd')) return;
 
-    db.getUserData(username).then((data) => {
-      if (hash.compare(old_password, data.password)) {
-        db.updateUser(username, 'password', new_password).then(() => {
-          res.redirect(`/profile?res=Passwort geändert`);
-        });
-      } else {
-        res.redirect(`/profile?res=Falsches Passwort`);
-      }
-    });
-  }
+  var username = req.session.username;
+  var old_password = req.body.password_old;
+  var new_password = req.body.password_new;
+
+  db.getUserData(username).then((data) => {
+    if (hash.compare(old_password, data.password)) {
+      db.updateUser(username, 'password', new_password).then(() => {
+        res.redirect(` / profile ? res = Passwort geändert `);
+      });
+    } else {
+      res.redirect(` / profile ? res = Falsches Passwort `);
+    }
+  });
 });
 
 app.post('/del-account', function(req, res) {
-  if (check_login(req, res)) {
-    var username = req.session.username;
-    var password = req.body.password;
+  if (!loggedin(req, res, 'del-account')) return;
 
-    db.getUserData(username).then((data) => {
-      if (hash.compare(password, data.password)) {
-        db.deleteUser(username).then(() => {
-          res.redirect(`/logout`);
-        });
-      } else {
-        res.redirect(`/profile?res=Falsches Passwort`);
-      }
-    });
-  }
+  var username = req.session.username;
+  var password = req.body.password;
+
+  db.getUserData(username).then((data) => {
+    if (hash.compare(password, data.password)) {
+      db.deleteUser(username).then(() => {
+        res.redirect(` / logout `);
+      });
+    } else {
+      res.redirect(` / profile ? res = Falsches Passwort `);
+    }
+  });
 });
